d8e543bda23cb026cb903b380b4c14a4f4275511
jayant-65 <jayantprajapat65@gmail.com>
2026-08-05 19:34:38 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'role-app-backend-production.up.railway.app'
+  baseURL: 'https://role-app-backend-production.up.railway.app/api'
 });
 
 api.interceptors.request.use((config) => {
